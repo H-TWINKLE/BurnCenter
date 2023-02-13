24eff9bacb9676c9abab57ae3fa7e38f0fd96d15
TWINKLE <zhanghjqqqq@163.com>
2023-02-13 21:37:01 +0800

    刻录中心初始化

---
+++ src/main/java/com/b/App.java
@@ -0,0 +1,10 @@
+package com.b;
+
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@EnableSpringDataWebSupport
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
