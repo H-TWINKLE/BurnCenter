c630496cb067915db4d614f2cb34bd2fe27fba75
TWINKLE <zhanghjqqqq@163.com>
2024-03-17 20:29:13 +0800

    依赖包模块拆分

--- src/main/java/com/b/App.java
+++
@@ -1,9 +0,0 @@
-package com.b;
-
-import cn.hutool.http.HttpRequest;
-
-public class App {
-    public static void main(String[] args) {
-        //HttpRequest.of("").execute().bodyBytes()
-    }
-}
